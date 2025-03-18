8c18ce452128bcf523d72d353057b2a3902def67
Jun wang <wang.jun6@northeastern.edu>
2025-03-17 21:03:16 -0600

    first commit

---
+++ expense-tracker/src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme";
+import GlobalState from "./context";
+
+createRoot(document.getElementById("root")).render(
+  <GlobalState>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </GlobalState>
+);
+
+
+
+
